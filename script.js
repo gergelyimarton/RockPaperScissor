23075eef999ce0b3cf52d4f088f4b16a9d30d846
Gergelyi Márton <gergelyimarci@gmail.com>
2023-02-05 01:20:14 +0000

    fixes

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-let userName = prompt("What's your name?")
+let nameIsRight = false
 let userWins = 0,
 opponentWins = 0;
 const rounds = 5;
+let nameOfPlayer = ""
 
-//legyen egy funkcio ami nem engedi hogy bot, paper, scissor, rock legyen a neved
+//legyen egy funkcio ami nem engedi hogy bot, paper, scissor, rock legyen a neved (megoldva)
 //legyen megoldva a jatek elinditasanak a kesleltetese hogy idoben lehessen megnyitni a console-t
 //legyen vege ha 3-nal tobb gyozelem van es irja ki a gyoztest
+//miert nem irja ki hogy hany ponttal gyozott?
 
-//this function decide who's the winner after the 5th round
-function theUltimateWinner (){
-    if(userWins < opponentWins){
-        console.log("Winner Winner, Chicken dinnner!\nBot is the Ultimate Winner!")
-    } else if(userWins = opponentWins){
-        console.log("It's a tie! No Ultimate Winner.")
+
+while (nameIsRight != true){
+    let userName = prompt("What's your name?")
+    if (userName !== "rock" && userName !== "paper" && userName !== "scissor" && userName !== "Rock" && userName !== "Paper" && userName !== "Scissor" && userName !== "Bot" && userName !== "bot"){
+        nameOfPlayer = userName
+        nameIsRight = true
     } else {
-        console.log("Winner Winner, Chicken dinner!\n" + userName + " is the Ultimate Winner!")
-    }  
+        alert("Please pick another name!")
+    }
 }
+
 //this function for the right input form
 function isItOkay (){
     let userChoise = prompt("Choose!\nRock, Paper or Scissor?");
@@ -26,7 +29,8 @@ function isItOkay (){
         return 0
     }
 
-    console.log(userName + " choose: " + lowerCaser)
+    
+    console.log(nameOfPlayer + " choose: " + lowerCaser)
     return lowerCaser;
 }
 //this function generate a random choice
@@ -54,20 +58,30 @@ function computerPlay() {
 //this function decide who's the winner after each round
 function whoTheWinner (playerSelection, computerSelection){
     if (playerSelection === "rock" && computerSelection === "scissor"){
-        userWins++;
-        return "You Win! Rock beats Scissor"
+        userWins += 1;
+        return "You Win! Rock beats Scissor\n--------------------------------------------------"
     } else if (playerSelection === "scissor" && computerSelection === "paper"){
-        userWins++;
-        return "You Win! Scissor beats Paper"
+        userWins += 1;
+        return "You Win! Scissor beats Paper\n--------------------------------------------------"
     } else if (playerSelection === "paper" && computerSelection === "rock"){
-        userWins++;
-        return "You Win! Paper beats Rock"
+        userWins += 1;
+        return "You Win! Paper beats Rock\n--------------------------------------------------"
     } else if (playerSelection === computerSelection){
-        return "It's a Tie!"
-    } else {
-        opponentWins++;
-        return "You lost! " + computerSelection + " beats " + playerSelection
+        return "It's a Tie!\n--------------------------------------------------"
+    } else { 
+        opponentWins += 1;
+        return "You lost! " + computerSelection + " beats " + playerSelection + "\n--------------------------------------------------"
     }
+}
+//this function decide who's the winner after the 5th round
+function theUltimateWinner (){
+    if(userWins < opponentWins){
+        alert("Winner Winner, Chicken dinnner!\nBot is the Ultimate Winner! With: " + opponentWins + " points")
+    } else if(userWins = opponentWins){
+        alert("It's a tie! No Ultimate Winner.")
+    } else {
+        alert("Winner Winner, Chicken dinner!\n" + nameOfPlayer + " is the Ultimate Winner! With: " + userWins + " points")
+    }  
 }
 //this is the game
 function gameStart (){
@@ -79,7 +93,7 @@ function gameStart (){
         while(itsOkay != true){
             playerChoice = isItOkay();
             if (playerChoice === 0){
-                console.log("Unknown input. Please choose again!");
+                alert("Unknown input.\nPlease choose again!");
             } else {
                 itsOkay = true;
             }
