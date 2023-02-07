3ea5cca5fbb60736c90311b3aa033cd669b8131e
Gergelyi Márton <gergelyimarci@gmail.com>
2023-02-07 20:28:47 +0000

    its working as we want, and kinda finished
    We need a design for this!

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ opponentWins = 0;
 const rounds = 5;
 let nameOfPlayer = ""
 
-//legyen egy funkcio ami nem engedi hogy bot, paper, scissor, rock legyen a neved (megoldva)
+//legyen egy funkcio ami nem engedi hogy bot, paper, scissor, rock legyen a neved (done!)
 //legyen megoldva a jatek elinditasanak a kesleltetese hogy idoben lehessen megnyitni a console-t
 //legyen vege ha 3-nal tobb gyozelem van es irja ki a gyoztest
-//miert nem irja ki hogy hany ponttal gyozott?
+//miert nem irja ki hogy hany ponttal gyozott? (done!)
 
 
 while (nameIsRight != true){
@@ -19,7 +19,6 @@ while (nameIsRight != true){
         alert("Please pick another name!")
     }
 }
-
 //this function for the right input form
 function isItOkay (){
     let userChoise = prompt("Choose!\nRock, Paper or Scissor?");
@@ -33,7 +32,7 @@ function isItOkay (){
     console.log(nameOfPlayer + " choose: " + lowerCaser)
     return lowerCaser;
 }
-//this function generate a random choice
+//this function generate a random choice 
 function computerPlay() {
     let randomChoose = Math.floor(Math.random() * 3);
     let letsChoice = ""
@@ -58,29 +57,34 @@ function computerPlay() {
 //this function decide who's the winner after each round
 function whoTheWinner (playerSelection, computerSelection){
     if (playerSelection === "rock" && computerSelection === "scissor"){
-        userWins += 1;
+        userWins++;
+        console.log(userWins)
         return "You Win! Rock beats Scissor\n--------------------------------------------------"
     } else if (playerSelection === "scissor" && computerSelection === "paper"){
-        userWins += 1;
+        userWins++;
+        console.log(userWins)
         return "You Win! Scissor beats Paper\n--------------------------------------------------"
     } else if (playerSelection === "paper" && computerSelection === "rock"){
-        userWins += 1;
+        userWins++;
+        console.log(userWins)
         return "You Win! Paper beats Rock\n--------------------------------------------------"
     } else if (playerSelection === computerSelection){
+        console.log(userWins, opponentWins)
         return "It's a Tie!\n--------------------------------------------------"
     } else { 
-        opponentWins += 1;
+        opponentWins++;
+        console.log(opponentWins)
         return "You lost! " + computerSelection + " beats " + playerSelection + "\n--------------------------------------------------"
     }
 }
 //this function decide who's the winner after the 5th round
 function theUltimateWinner (){
     if(userWins < opponentWins){
-        alert("Winner Winner, Chicken dinnner!\nBot is the Ultimate Winner! With: " + opponentWins + " points")
-    } else if(userWins = opponentWins){
+        alert("Winner Winner, Chicken dinnner!\nBot is the Ultimate Winner!\nWith: " + opponentWins + " points")
+    } else if(userWins == opponentWins){
         alert("It's a tie! No Ultimate Winner.")
     } else {
-        alert("Winner Winner, Chicken dinner!\n" + nameOfPlayer + " is the Ultimate Winner! With: " + userWins + " points")
+        alert("Winner Winner, Chicken dinner!\n" + nameOfPlayer + " is the Ultimate Winner\nWith: " + userWins + " points")
     }  
 }
 //this is the game
